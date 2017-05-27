1eee971d387e1277df6b4961fe0a0d93d8d7d742
mark <mmansi@utexas.edu>
2017-05-27 16:36:28 -0400

    better chaos_wrapper.sh is less conducive to attack

--- chaos_wrapper.sh
+++ chaos_wrapper.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 function rollback() {
-    git checkout master
-    git checkout HEAD^
+    # avoid commits from the same faulty PR
+    rb_commit=$(git log --author chaosbot --format=format:%H | tail -n +2 | head -n 1)
+    echo "Rollback to commit $rb_commit" >&2
+    git reset --hard $rb_commit
 }
 
-/root/.virtualenvs/chaos/bin/python chaos.py || rollback
+# time the chaos server... if it crashes in 60s, then attempt a rollback
+start_time=`date +%s`
+/root/.virtualenvs/chaos/bin/python chaos.py 
+failed=$?
+time_elasped=`expr $(date +%s) - $start_time`
+
+if [ "$failed" -eq 0 ] && [ "$time_elasped" -le 60 ]; then
+    echo "Crashed in less than 60 seconds!" >&2
+    rollback
+fi
