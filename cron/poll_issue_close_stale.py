7a817a067c132ca32fe092173d0cd7d2d5f6fa2f
mark <mmansi@utexas.edu>
2017-05-29 19:07:45 -0400

    Need to use issue # not id

--- cron/poll_issue_close_stale.py
+++ cron/poll_issue_close_stale.py
@@ -24,19 +24,22 @@ def poll_issue_close_stale():
     __log.info("There are currently %d open issues" % len(issues))
 
     for issue in issues:
+        number = issue["number"]
         last_updated = arrow.get(issue["updated_at"])
 
         now = arrow.utcnow()
         delta = (now - last_updated).total_seconds()
 
+        __log.info("Issue %d has not been updated in %d seconds" % (number, delta))
+
         if delta > settings.ISSUE_STALE_THRESHOLD:
-            __log.info("Vote close issue %d" % issue["id"])
+            __log.info("/vote close issue %d" % number)
 
             # leave an explanatory comment
             body = "This issue hasn't been active for a while." + \
                     "To keep it open, react with :-1: on the `vote close` post."
-            gh.comments.leave_comment(api, settings.URN, issue["id"], body)
+            gh.comments.leave_comment(api, settings.URN, number, body)
 
             # then vote to close
             body = "/vote close"
-            gh.comments.leave_comment(api, settings.URN, issue["id"], body)
+            gh.comments.leave_comment(api, settings.URN, number, body)
